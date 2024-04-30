10c3e2a8600bfe8cd8f6f708d64a44003843facf
SandraHartfelder <shartfelder@web.de>
2024-04-30 14:04:10 +0200

    Update

--- A03_Einkaufsliste/code.ts
+++ A03_Einkaufsliste/code.ts
@@ -1,26 +1,26 @@
 document.getElementById('addRowButton').addEventListener('click', function() {
-    var table = document.getElementById("myTable");
-    var newRow = table.insertRow(table.rows.length); 
+    let table = document.getElementById("myTable");
+    let newRow = table.insertRow(table.rows.length); 
 
-    for (var i = 0; i < 7; i++) { 
-        var cell = newRow.insertCell(i); 
+    for (let i = 0; i < 7; i++) { 
+        let cell = newRow.insertCell(i); 
         cell.contentEditable = true; 
 
         if (i < 4) { 
             cell.textContent = "Neuer Eintrag"; 
         } else if (i === 4 || i === 5) { 
-            var checkbox = document.createElement('input'); 
+            let checkbox = document.createElement('input'); 
             checkbox.type = "checkbox"; 
             cell.innerHTML = ''; 
             cell.appendChild(checkbox); 
         }
 
         if (i === 6) { 
-            var deleteButton = document.createElement('button');
+            let deleteButton = document.createElement('button');
             deleteButton.textContent = 'Löschen';
             deleteButton.classList.add('deleteButton'); 
             deleteButton.onclick = function() {
-                var row = this.parentNode.parentNode;
+                let row = this.parentNode.parentNode;
                 row.parentNode.removeChild(row);
             };
             cell.appendChild(deleteButton);
